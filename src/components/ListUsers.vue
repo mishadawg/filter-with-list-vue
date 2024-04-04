6b5bb3828635bd77dec361404e8275bb1ec34d99
misha_runner <misha40401@yandex.ru>
2024-04-04 12:47:21 +0300

    layout of components and view.

---
+++ src/components/ListUsers.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>list items</h1>
+    <div>
+      <UserCard />
+    </div>
+  </div>
+</template>
+<script>
+import UserCard from "@/components/UserCard";
+
+export default {
+  name: "ListUsers",
+  components: {
+    UserCard,
+  },
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Filter />
+    <ListUsers />
+    <ShowMoreTrigger />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Filter from "@/components/Filter";
+import ListUsers from "@/components/ListUsers";
+import ShowMoreTrigger from "@/components/ShowMoreTrigger";
 
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+    Filter,
+    ListUsers,
+    ShowMoreTrigger,
   },
 };
 </script>
