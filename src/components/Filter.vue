d9e15c81430c028dccfa2c86a2b432a1a4772b63
misha_runner <misha40401@yandex.ru>
2024-04-04 13:55:23 +0300

    filter html.

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -1,3 +1,34 @@
 <template>
-  <div>filter</div>
+  <div class="filter">
+    <div class="filter-input">
+      <input type="text" v-model="text" />
+    </div>
+    <div class="filter-input">
+      <select v-model="select">
+        <option value="active">All</option>
+        <option value="active">Active</option>
+        <option value="blocked">Blocked</option>
+      </select>
+    </div>
+  </div>
 </template>
+<script>
+export default {
+  name: "Filter",
+  data() {
+    return {
+      text: "",
+      select: null,
+    };
+  },
+};
+</script>
+<style lang="scss" scoped>
+.filter {
+  display: flex;
+  align-items: center;
+  gap: 16px 25px;
+  flex-wrap: wrap;
+  margin-bottom: 16px;
+}
+</style>
